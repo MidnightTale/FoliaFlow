be40f3e42be48335e306e4522feac410c94a1997
MidnightTale <midnighttale24@gmail.com>
2023-04-13 12:51:36 +0700

    vanilla velocity 2

--- src/main/java/xyz/hynse/foliaflow/FoliaFlow.java
+++ src/main/java/xyz/hynse/foliaflow/FoliaFlow.java
@@ -21,8 +21,8 @@ import java.util.concurrent.TimeUnit;
 import static org.bukkit.Bukkit.getScheduler;
 
 public class FoliaFlow extends JavaPlugin implements Listener {
-    private final double vh = 0.1;
-    private final double vt = 0.3;
+    private final double vh = 0.3;
+    private final double vt = 0.8;
     private final Vector velocity1 = new Vector(0, vh, vt);
     private final Vector velocity2 = new Vector(vt, vh, 0);
     private final Vector velocity3 = new Vector(0, vh, -vt);
