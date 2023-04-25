758b5dc8fc7d43d4ee25745887f00596d0202b03
Sofiane H. Djerbi <46628754+kugge@users.noreply.github.com>
2023-04-25 23:40:47 +0300

    Refactoring / Optimize project

---
+++ src/main/java/xyz/hynse/foliaflow/watcher/PortalWatcher.java
@@ -0,0 +1,45 @@
+package xyz.hynse.foliaflow.watcher;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.util.Vector;
+import xyz.hynse.foliaflow.FoliaFlow;
+
+import static xyz.hynse.foliaflow.util.VelocityUtil.getBlockMovingTo;
+
+public class PortalWatcher implements Listener {
+    @EventHandler
+    public void onFallingBlockToBlock(EntityChangeBlockEvent e){
+        if(e.getEntityType() != EntityType.FALLING_BLOCK) return;
+        Entity entity = e.getEntity();
+        Location loc = entity.getLocation();
+        Vector vel = entity.getVelocity();
+        Block movingTo = getBlockMovingTo(loc, vel);
+
+        if(movingTo != null && movingTo.getType() == Material.END_PORTAL){
+            Location spawnLoc = movingTo.getLocation();
+            spawnLoc.add(0.5, 0.5, 0.5);
+
+            FallingBlock dummy = loc.getWorld().spawnFallingBlock(spawnLoc, ((FallingBlock) entity).getBlockData());
+            Vector dummyVel = vel.clone();
+            dummyVel.setY(-dummyVel.getY());
+            dummyVel.multiply(1.3); // This seems vanilla.
+            dummyVel.add(new Vector(0, -0.2, 0));
+            dummy.setVelocity(dummyVel);
+
+            // Add vector seems vanilla.
+            dummy.getScheduler().runDelayed(FoliaFlow.instance,
+                task -> dummy.setVelocity(dummyVel.add(new Vector(0, 0.45, 0))),
+                null,
+                2
+            );
+        }
+    }
+}
